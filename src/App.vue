00cf4aa2e6f7027abe8a10f6ed6597fcfd41fde0
lingy-l <237629724@qq.com>
2019-08-29 11:37:47 +0800

    init sell项目重启

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <index></index>
+</template>
+
+<script>
+import index from './pages/Index'
+export default {
+  name: 'app',
+  components: {
+    index
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import router from './router'
+import App from './App.vue'
+import iview from 'iview'
+import 'iview/dist/styles/iview.css'
+
+Vue.use(iview)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
