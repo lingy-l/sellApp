168e5c434ac49476ddb9568a52565160d131b109
lingy-l <237629724@qq.com>
2019-08-30 17:53:57 +0800

    首页更新

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import router from './router'
 import App from './App.vue'
 import iview from 'iview'
 import 'iview/dist/styles/iview.css'
+import { Lazyload } from 'vant';
 
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+Vue.use(Lazyload);
 Vue.use(iview)
 
 Vue.config.productionTip = false
